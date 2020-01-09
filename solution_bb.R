9cc41f867f64b14a490988390c136818b4f9aa03
Michi-Parz <michael.parzinger@web.de>
2020-01-09 17:43:36 +0100

    Mabacht dabas wabas ebes tubun soboll.

--- solution_bb.R
+++ solution_bb.R
@@ -1,37 +1,38 @@
 new_bb <- function(text) {
-  checkmate::assert_character(text)
-  
   # replace ((consonants) (1-2 vowels) (consonants)) with
   # ((consonants) (vowels) b (same vowels again) (consonants)):
   match <- "([^aeiouäöüAEIOUÄÜÖ]*)([aeiouäöü]{1,2})([^aeiouäöü]*)"
   bb <- gsub(pattern = match, replacement = "\\1\\2b\\2\\3", x = text)
-  
+
   structure(bb, class = "bb")
 }
 
 
 bb <- function(object, ...) UseMethod("bb")
 
-bb.default <- function(text){
+bb.default <- function(text) {
+  if (!checkmate::test_character(text)) {
+    stop(bb("non-character objects cannot be turned into bb-objects!"))
+  }
   new_bb(text)
 }
 
-bb.list <- function(list){
-  structure(lapply(list,bb), class = "bb")
+bb.list <- function(list) {
+  structure(lapply(list, bb), class = "bb")
 }
 
-bb.factor <- function(x){
+bb.factor <- function(x) {
   char <- as.character(x)
   structure(
     factor(bb(char), levels = bb(sort(char))),
-    class = c("bb","factor")
+    class = c("bb", "factor")
   )
 }
 
-bb.ordered <- function(x){
+bb.ordered <- function(x) {
   char <- as.character(x)
   structure(
     ordered(bb(char), levels = bb(sort(char))),
-    class = c("bb","ordered")
+    class = c("bb", "ordered")
   )
 }
